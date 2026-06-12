247be71ce8d0f39f53b543b28e666ad860739980
yksaini9928 <yksaini15699@.com>
2026-06-12 12:17:52 +0530

    python

---
+++ python/7.1-Regular-Expression/7.1-reg-exp.py
@@ -0,0 +1,9 @@
+message ="the current python version is 3.13"
+
+#if pyhhon is persent in message then print the message
+
+print("python" in message)
+print("3.13" in message)
+print("3.14" in message)
+
+print(message.find("python"))
